b335ea382d1696de732fc944ea8a41ee985f9a04
Makhmud <makhmudkuzikaev@gmail.com>
2023-09-25 14:10:04 +0600

    finish project

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgFor} from '@angular/common';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/Column.model';
 
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
   styleUrls: ['./main-view.component.scss']
 })
-export class MainViewComponent {
+export class MainViewComponent implements OnInit {
 
+  /**
+   *
+   */
+  constructor() {
+    
+  }
+
+  ngOnInit() {
+  }
+
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      "Some random idea",
+      "This is another random idea",
+      "Build an awesome application"
+    ]),
+    new Column('Research', [
+      "Lorem ipsum",
+      "foo",
+      "This was in the 'Research' column"
+    ]),
+    new Column('Todo', [
+      "Get to work",
+      "Pick up grocerles",
+      "Go home",
+      "Fall asleep"
+    ]),
+    new Column('Done', [
+      'Get up', 
+      'Brush teeth', 
+      'Take a shower', 
+      'Check e-mail', 
+      'Walk dog'
+    ])
+  ]);
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
